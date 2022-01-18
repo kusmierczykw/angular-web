5f8a3006a94214d819e46c414ff5e7f0996bee30
Wojciech Kuśmierczyk <wojciech.kusmierczyk@outlook.com>
2022-01-18 07:46:28 +0100

    Create of form.

--- src/app/features/training/components/training-form/training-form.module.ts
+++ src/app/features/training/components/training-form/training-form.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TrainingFormComponent } from './training-form.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [TrainingFormComponent],
   exports: [TrainingFormComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
 })
 export class TrainingFormModule {}
