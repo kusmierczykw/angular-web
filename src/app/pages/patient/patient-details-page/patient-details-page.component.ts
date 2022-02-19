db7e60cc0a1e3649698b83f035066fd75eeba6cc
Wojciech Kuśmierczyk <wojciech.kusmierczyk@outlook.com>
2022-02-19 13:03:53 +0100

    Initialize and free breadcrumb variables.

--- src/app/pages/patient/patient-details-page/patient-details-page.component.ts
+++ src/app/pages/patient/patient-details-page/patient-details-page.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { TextValueProviderService } from '@shared/text-variables/services/text-value-provider.service';
 import { RouteBreadcrumbVariable } from '@core/routing/data/route-breadcrumb-variable';
 import { toObservable } from '@utils/rxjs/operators';
@@ -8,17 +8,30 @@ import { toObservable } from '@utils/rxjs/operators';
   templateUrl: './patient-details-page.component.html',
   styleUrls: ['./patient-details-page.component.scss'],
 })
-export class PatientDetailsPageComponent implements OnInit {
+export class PatientDetailsPageComponent implements OnInit, OnDestroy {
   public constructor(
     private readonly textValueProvider: TextValueProviderService,
   ) {}
 
   public ngOnInit(): void {
+    this.initializeBreadcrumbVariables();
+  }
+
+  public ngOnDestroy(): void {
+    this.freeBreadcrumbsVariables();
+  }
+
+  private initializeBreadcrumbVariables(): void {
     this.textValueProvider.setValueFor(RouteBreadcrumbVariable.FIRST_NAME, () =>
       toObservable('Wojciech'),
     );
     this.textValueProvider.setValueFor(RouteBreadcrumbVariable.LAST_NAME, () =>
       toObservable('Kuśmierczyk'),
     );
+  }
+
+  private freeBreadcrumbsVariables(): void {
+    this.textValueProvider.clearValueFor(RouteBreadcrumbVariable.FIRST_NAME);
+    this.textValueProvider.clearValueFor(RouteBreadcrumbVariable.LAST_NAME);
   }
 }
