0de5010893765ebe42d8273014ed32cd5570351d
Wojciech Kuśmierczyk <wojciech.kusmierczyk@outlook.com>
2022-05-05 21:43:09 +0200

    Temporary remove overlay.

--- src/app/shared/tables/components/quick-table-renderer/fragments/header-cell-content/header-cell-content.module.ts
+++ src/app/shared/tables/components/quick-table-renderer/fragments/header-cell-content/header-cell-content.module.ts
@@ -1,11 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderCellContentComponent } from './header-cell-content.component';
-import { HeaderActionOverlayModule } from '@shared/tables/components/quick-table-renderer/fragments/header-action-overlay/header-action-overlay.module';
 
 @NgModule({
   declarations: [HeaderCellContentComponent],
   exports: [HeaderCellContentComponent],
-  imports: [CommonModule, HeaderActionOverlayModule],
+  imports: [CommonModule],
 })
 export class HeaderCellContentModule {}
